3a4832bd9e4bd2fc57f761011c235dcc88d49745
Blair Habicht <blairh@jellycode.com>
2022-08-28 10:56:13 -0600

    Update index.js with constructor inheriting Mix context

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const mix = require('laravel-mix')
-const JavaScript = require('laravel-mix/src/components/JavaScript')
-const DependencyExtractionPlugin = require('@wordpress/dependency-extraction-webpack-plugin')
+const mix = require('laravel-mix');
+const JavaScript = require('laravel-mix/src/components/JavaScript');
+const DependencyExtractionPlugin = require('@wordpress/dependency-extraction-webpack-plugin');
 
 /**
  * Laravel Mix WP Block
@@ -9,70 +9,75 @@ const DependencyExtractionPlugin = require('@wordpress/dependency-extraction-web
  * @see https://www.npmjs.com/package/@wordpress/dependency-extraction-webpack-plugin
  */
 class Block extends JavaScript {
-  name() {
-    return ['blocks', 'block']
-  }
-  /**
-   * All dependencies that should be installed by Mix.
-   *
-   * @return {array}
-   */
-  dependencies() {
-    this.requiresReload = `
+ constructor() {
+  super();
+  this.context = Mix;
+ }
+ name() {
+  return ['blocks', 'block'];
+ }
+ /**
+  * All dependencies that should be installed by Mix.
+  *
+  * @return {array}
+  */
+ dependencies() {
+  this.requiresReload = `
       Dependencies have been installed. Please run again.
-    `
+    `;
 
-    return [
-      '@wordpress/babel-preset-default',
-      '@wordpress/dependency-extraction-webpack-plugin',
-    ]
-  }
+  return [
+   '@wordpress/babel-preset-default',
+   '@wordpress/dependency-extraction-webpack-plugin',
+  ];
+ }
 
-  /**
-   * Register the plugin component.
-   *
-   * @param  {string} entry
-   * @param  {string} output
-   * @param  {object} options
-   * @return {void}
-   */
-  register(entry, output, options = {}) {
-    this.pluginOptions = (
-      options.disableRegenerator === true ? {
-        ...options,
-        requestToExternal: function(request) {
-          if (request === '@babel/runtime/regenerator') {
-            return null
-          }
-        },
-      } : options
-    )
+ /**
+  * Register the plugin component.
+  *
+  * @param  {string} entry
+  * @param  {string} output
+  * @param  {object} options
+  * @return {void}
+  */
+ register(entry, output, options = {}) {
+  this.pluginOptions =
+   options.disableRegenerator === true
+    ? {
+       ...options,
+       requestToExternal: function (request) {
+        if (request === '@babel/runtime/regenerator') {
+         return null;
+        }
+       },
+      }
+    : options;
 
-    super.register(entry, output)
-  }
+  super.register(entry, output);
+ }
 
-  /**
-   * Plugins to be merged with the master webpack config.
-   *
-   * @return {array|object}
-   */
-  webpackPlugins() {
-    return new DependencyExtractionPlugin({
-      ...this.pluginOptions,
-    })
-  }
+ /**
+  * Plugins to be merged with the master webpack config.
+  *
+  * @return {array|object}
+  */
+ webpackPlugins() {
+  return new DependencyExtractionPlugin({
+   ...this.pluginOptions,
+  });
+ }
 
-  /**
-   * Babel config to be merged with the master Babel config.
-   *
-   * @return {object}
-   */
-  babelConfig() {
-    return {
-      presets: ['@wordpress/babel-preset-default'],
-    }
-  }
+ /**
+  * Babel config to be merged with the master Babel config.
+  *
+  * @return {object}
+  */
+ babelConfig() {
+  return {
+   presets: ['@wordpress/babel-preset-default'],
+  };
+ }
 }
 
-mix.extend('block', new Block())
-mix.extend('blocks', new Block())
+mix.extend('block', new Block());
+mix.extend('blocks', new Block());
